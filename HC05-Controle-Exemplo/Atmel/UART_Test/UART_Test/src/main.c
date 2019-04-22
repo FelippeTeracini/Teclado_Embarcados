892ff7be52b29518439ec6e1f66acfe522788051
TeraByte900 <fe.n.teracini@hotmail.com>
2019-04-22 18:39:07 -0300

    volume

--- HC05-Controle-Exemplo/Atmel/UART_Test/UART_Test/src/main.c
+++ HC05-Controle-Exemplo/Atmel/UART_Test/UART_Test/src/main.c
@@ -30,6 +30,8 @@
  */
 #include <asf.h>
 #include <string.h>
+#include <assert.h>
+#include "asf.h"
 
 // Descomente o define abaixo, para desabilitar o Bluetooth e utilizar modo Serial via Cabo
 //#define DEBUG_SERIAL
@@ -65,10 +67,28 @@
 
 #define MAX_SIZE 32
 
+#define VOLT_REF        (3300)
+#define MAX_DIGITAL     (4095)
+
+/** The conversion data is done flag */
+volatile bool g_is_conversion_done = false;
+volatile bool g_is_res_done = false;
+
+/** The conversion data value */
+volatile uint32_t g_ul_value = 0;
+volatile uint32_t g_res_value = 0;
+
+volatile bool g_delay = false;
+
+/* Canal do sensor de temperatura */
+#define AFEC_CHANNEL_TEMP_SENSOR 2
+#define AFEC_CHANNEL_RES_PIN 5
+
 volatile long g_systimer = 0;
 volatile char lista[MAX_SIZE][3];
 volatile int n = 0;
 volatile char eop = '-';
+volatile int prev_volume;
 
 void BUT_RISE(char button){
 	
@@ -145,6 +165,180 @@ void SysTick_Handler() {
 	g_systimer++;
 }
 
+static void AFEC_Temp_callback(void)
+{
+	g_ul_value = afec_channel_get_value(AFEC0, AFEC_CHANNEL_TEMP_SENSOR);
+	g_is_conversion_done = true;
+}
+
+static void AFEC_Res_callback(void)
+{
+	g_res_value = afec_channel_get_value(AFEC0, AFEC_CHANNEL_RES_PIN);
+	g_is_res_done = true;
+}
+
+void TC1_Handler(void){
+	volatile uint32_t ul_dummy;
+
+	/****************************************************************
+	* Devemos indicar ao TC que a interrup??o foi satisfeita.
+	******************************************************************/
+	ul_dummy = tc_get_status(TC0, 1);
+
+	/* Avoid compiler warning */
+	UNUSED(ul_dummy);
+
+	afec_channel_enable(AFEC0, AFEC_CHANNEL_TEMP_SENSOR);
+	afec_start_software_conversion(AFEC0);
+
+}
+
+void TC0_Handler(void){
+	volatile uint32_t ul_dummy;
+
+	/****************************************************************
+	* Devemos indicar ao TC que a interrup??o foi satisfeita.
+	******************************************************************/
+	ul_dummy = tc_get_status(TC0, 0);
+
+	/* Avoid compiler warning */
+	UNUSED(ul_dummy);
+
+	afec_channel_enable(AFEC0, AFEC_CHANNEL_RES_PIN);
+	afec_start_software_conversion(AFEC0);
+	
+}
+
+/**
+ * converte valor lido do ADC para temperatura em graus celsius
+ * input : ADC reg value
+ * output: Temperature in celsius
+ */
+static int32_t convert_adc_to_temp(int32_t ADC_value){
+
+  int32_t ul_vol;
+  int32_t ul_temp;
+
+  /*
+   * converte bits -> tens?o (Volts)
+   */
+	ul_vol = ADC_value * VOLT_REF / (float) MAX_DIGITAL;
+
+  /*
+   * According to datasheet, The output voltage VT = 0.72V at 27C
+   * and the temperature slope dVT/dT = 2.33 mV/C
+   */
+  ul_temp = (ul_vol - 720)  * 100 / 233 + 27;
+  return(ul_temp);
+}
+
+static int32_t convert_adc_to_res(int32_t ADC_value){
+
+  int32_t ul_vol;
+  int32_t ul_res;
+
+  /*
+   * converte bits -> tens?o (Volts)
+   */
+	ul_vol = ADC_value * VOLT_REF / (float) MAX_DIGITAL;
+
+  /*
+   * According to datasheet, The output voltage VT = 0.72V at 27C
+   * and the temperature slope dVT/dT = 2.33 mV/C
+   */
+  ul_res = (ul_vol - 720)  * 100 / 233 + 27; //MUDAR
+  return(ul_res);
+}
+
+static int32_t convert_adc_to_axis(int32_t ADC_value){
+	
+	return (ADC_value-2200);
+}
+
+static int32_t convert_adc_to_volume(int32_t ADC_value){
+	
+	return (ADC_value*100 / MAX_DIGITAL);
+}
+
+static void config_ADC_TEMP_RES(void){
+/*************************************
+   * Ativa e configura AFEC
+   *************************************/
+  /* Ativa AFEC - 0 */
+	afec_enable(AFEC0);
+
+	/* struct de configuracao do AFEC */
+	struct afec_config afec_cfg;
+
+	/* Carrega parametros padrao */
+	afec_get_config_defaults(&afec_cfg);
+
+	/* Configura AFEC */
+	afec_init(AFEC0, &afec_cfg);
+
+	/* Configura trigger por software */
+	afec_set_trigger(AFEC0, AFEC_TRIG_SW);
+
+	/* configura call back */
+	afec_set_callback(AFEC0, AFEC_INTERRUPT_EOC_2,	AFEC_Temp_callback, 1);
+	afec_set_callback(AFEC0, AFEC_INTERRUPT_EOC_5,	AFEC_Res_callback, 1);
+
+	/*** Configuracao espec?fica do canal AFEC ***/
+	struct afec_ch_config afec_ch_cfg;
+	afec_ch_get_config_defaults(&afec_ch_cfg);
+	afec_ch_cfg.gain = AFEC_GAINVALUE_0;
+	afec_ch_set_config(AFEC0, AFEC_CHANNEL_TEMP_SENSOR, &afec_ch_cfg);
+
+	/*
+	* Calibracao:
+	* Because the internal ADC offset is 0x200, it should cancel it and shift
+	 down to 0.
+	 */
+	afec_channel_set_analog_offset(AFEC0, AFEC_CHANNEL_TEMP_SENSOR, 0x200);
+	afec_channel_set_analog_offset(AFEC0, AFEC_CHANNEL_RES_PIN, 0x200);
+
+	/***  Configura sensor de temperatura ***/
+	struct afec_temp_sensor_config afec_temp_sensor_cfg;
+
+	afec_temp_sensor_get_config_defaults(&afec_temp_sensor_cfg);
+	afec_temp_sensor_set_config(AFEC0, &afec_temp_sensor_cfg);
+
+	/* Selecina canal e inicializa convers?o */
+	//afec_channel_enable(AFEC0, AFEC_CHANNEL_TEMP_SENSOR);
+	//afec_channel_enable(AFEC0, AFEC_CHANNEL_RES_PIN);
+}
+
+
+
+void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq){
+	uint32_t ul_div;
+	uint32_t ul_tcclks;
+	uint32_t ul_sysclk = sysclk_get_cpu_hz();
+
+	uint32_t channel = 1;
+
+	/* Configura o PMC */
+	/* O TimerCounter ? meio confuso
+	o uC possui 3 TCs, cada TC possui 3 canais
+	TC0 : ID_TC0, ID_TC1, ID_TC2
+	TC1 : ID_TC3, ID_TC4, ID_TC5
+	TC2 : ID_TC6, ID_TC7, ID_TC8
+	*/
+	pmc_enable_periph_clk(ID_TC);
+
+	/** Configura o TC para operar em  4Mhz e interrup?c?o no RC compare */
+	tc_find_mck_divisor(freq, ul_sysclk, &ul_div, &ul_tcclks, ul_sysclk);
+	tc_init(TC, TC_CHANNEL, ul_tcclks | TC_CMR_CPCTRG);
+	tc_write_rc(TC, TC_CHANNEL, ((ul_sysclk) / ul_div) / freq);
+
+	/* Configura e ativa interrup?c?o no TC canal 0 */
+	/* Interrup??o no C */
+	NVIC_EnableIRQ((IRQn_Type) ID_TC);
+	tc_enable_interrupt(TC, TC_CHANNEL, TC_IER_CPCS);
+
+	/* Inicializa o canal 0 do TC */
+	tc_start(TC, TC_CHANNEL);
+}
 
 void config_console(void) {
 	usart_serial_options_t config;
@@ -155,6 +349,17 @@ void config_console(void) {
 	usart_serial_init(USART1, &config);
 	usart_enable_tx(USART1);
 	usart_enable_rx(USART1);
+	
+	const usart_serial_options_t uart_serial_options = {
+		.baudrate   = CONF_UART_BAUDRATE,
+		.charlength = CONF_UART_CHAR_LENGTH,
+		.paritytype = CONF_UART_PARITY,
+		.stopbits   = CONF_UART_STOP_BITS,
+	};
+
+	/* Configure console UART. */
+	sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
+	stdio_serial_init(CONF_UART, &uart_serial_options);
 }
 
 void usart_put_string(Usart *usart, char str[]) {
@@ -233,6 +438,10 @@ int main (void)
 	delay_init();
 	SysTick_Config(sysclk_get_cpu_hz() / 1000); // 1 ms
 	config_console();
+	config_ADC_TEMP_RES();
+	
+	TC_init(TC0, ID_TC1, 1, 1);
+	TC_init(TC0, ID_TC0, 0, 10);
 	
 	#ifndef DEBUG_SERIAL
 	usart_put_string(USART1, "Inicializando...\r\n");
@@ -242,6 +451,13 @@ int main (void)
 	#endif
 	
 	char buffer[1024];
+	
+	afec_channel_enable(AFEC0, AFEC_CHANNEL_TEMP_SENSOR);
+	afec_start_software_conversion(AFEC0);
+	if (g_is_conversion_done){
+		afec_channel_enable(AFEC0, AFEC_CHANNEL_RES_PIN);
+		afec_start_software_conversion(AFEC0);
+	}
 	
 	while(1) {
 		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
@@ -255,6 +471,24 @@ int main (void)
 			}
 			n--;
 
+		}
+		
+		if (g_is_conversion_done==true){
+			
+			int volume = convert_adc_to_volume(g_res_value);
+			if(volume != prev_volume){
+				printf("Volume: %d\n", volume);
+				char volume_char = volume;
+				lista[n][0] = 'v';
+				lista[n][1] = volume_char;
+				lista[n][2] = eop;
+				n++;	
+			}
+			prev_volume = volume;
+			g_is_conversion_done = false;
+		}
+		if(g_is_res_done==true){
+			g_is_res_done = false;
 		}
 	}
 }
--- HC05-Controle-Exemplo/PC_Python/youtube_controller.py
+++ HC05-Controle-Exemplo/PC_Python/youtube_controller.py
@@ -3,10 +3,31 @@ import serial
 import argparse
 import time
 import logging
+from __future__ import print_function
+from pycaw.pycaw import AudioUtilities
+
+
+class AudioController(object):
+    def __init__(self, process_name):
+        self.process_name = process_name
+        self.volume = self.process_volume()
+
+    def set_volume(self, decibels):
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            interface = session.SimpleAudioVolume
+            if session.Process and session.Process.name() == self.process_name:
+                # only set volume in the range 0.0 to 1.0
+                self.volume = min(1.0, max(0.0, decibels))
+                interface.SetMasterVolume(self.volume, None)
+                print('Volume set to', self.volume)  # debug
+
 
 class MyControllerMap:
     def __init__(self):
-        self.button = {'q': 'q', 'w': 'w', 'e': 'e'} # Fast forward (10 seg) pro Youtube
+        # Fast forward (10 seg) pro Youtube
+        self.button = {'q': 'q', 'w': 'w', 'e': 'e'}
+
 
 class SerialControllerInterface:
     # Protocolo
@@ -17,10 +38,13 @@ class SerialControllerInterface:
         self.ser = serial.Serial(port, baudrate=baudrate)
         self.mapping = MyControllerMap()
         self.incoming = '0'
-        pyautogui.PAUSE = 0  ## remove delay
-    
+        pyautogui.PAUSE = 0  # remove delay
+
     def update(self):
-        ## Sync protocol
+        # Sync protocol
+        audio_controller = AudioController('chrome.exe')
+        audio_controller.set_volume(0.0)
+
         while self.incoming != b'-':
             self.incoming = self.ser.read()
             logging.debug("Received INCOMING: {}".format(self.incoming))
@@ -36,7 +60,7 @@ class SerialControllerInterface:
             if status == b'1':
                 logging.info("KEYDOWN q")
                 pyautogui.keyDown(self.mapping.button['q'])
-                
+
             elif status == b'0':
                 logging.info("KEYUP q")
                 pyautogui.keyUp(self.mapping.button['q'])
@@ -46,7 +70,7 @@ class SerialControllerInterface:
             if status == b'1':
                 logging.info("KEYDOWN w")
                 pyautogui.keyDown(self.mapping.button['w'])
-                
+
             elif status == b'0':
                 logging.info("KEYUP w")
                 pyautogui.keyUp(self.mapping.button['w'])
@@ -56,10 +80,14 @@ class SerialControllerInterface:
             if status == b'1':
                 logging.info("KEYDOWN e")
                 pyautogui.keyDown(self.mapping.button['e'])
-                
+
             elif status == b'0':
                 logging.info("KEYUP e")
                 pyautogui.keyUp(self.mapping.button['e'])
+
+        if button == b'v':
+            volume = int(status) / 100
+            audio_controller.set_volume(volume)
 
         self.incoming = self.ser.read()
         print(self.incoming)
@@ -82,17 +110,20 @@ if __name__ == '__main__':
     argparse = argparse.ArgumentParser()
     argparse.add_argument('serial_port', type=str)
     argparse.add_argument('-b', '--baudrate', type=int, default=9600)
-    argparse.add_argument('-c', '--controller_interface', type=str, default='serial', choices=interfaces)
+    argparse.add_argument('-c', '--controller_interface',
+                          type=str, default='serial', choices=interfaces)
     argparse.add_argument('-d', '--debug', default=False, action='store_true')
     args = argparse.parse_args()
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
 
-    print("Connection to {} using {} interface ({})".format(args.serial_port, args.controller_interface, args.baudrate))
+    print("Connection to {} using {} interface ({})".format(
+        args.serial_port, args.controller_interface, args.baudrate))
     if args.controller_interface == 'dummy':
         controller = DummyControllerInterface()
     else:
-        controller = SerialControllerInterface(port=args.serial_port, baudrate=args.baudrate)
+        controller = SerialControllerInterface(
+            port=args.serial_port, baudrate=args.baudrate)
 
     while True:
         controller.update()
